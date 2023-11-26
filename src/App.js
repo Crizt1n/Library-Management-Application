e3a23669b116b7de2506973f8fb29d5bf0024f17
crizt1n <christin38thomas@gmail.com>
2023-11-26 23:29:46 +0530

    booklist modified(Error in deleting)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import BookList from './components/BookList'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Edit from './components/Edit'
+import AddBook from './components/AddBook';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/booklist' element={<BookList />}></Route>
+        <Route path='/addbook' element={<AddBook />}></Route>
+
         <Route path='/edit/:id' element={<Edit/>}></Route>
       </Routes></BrowserRouter>
   );
